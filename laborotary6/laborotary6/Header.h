86349dbdffc2c9e01532d2a171b35edb79fec50a
Артем <asushenya@mail.ru>
2015-10-04 18:37:14 +0300

    перегрузка операций

---
+++ laborotary6/laborotary6/Header.h
@@ -0,0 +1,235 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+HANDLE hConsole1 = GetStdHandle(STD_OUTPUT_HANDLE);
+
+class String
+{
+private : 
+    char *array;
+public :
+    String()
+    {
+        array = new char[256];
+        strcpy(array, "empty");
+    }
+    String(char*p)
+    {
+        array = new char[256];
+        strcpy(array, p);
+    }
+
+    String(String& c)
+    {
+        array = new char[256];
+        strcpy(this->array, c.array);
+    }
+
+    String(String *c)
+    {
+        strcpy(this->array, c->array);
+    }
+
+
+    ~String()
+   {
+       delete[] array;
+    }
+
+
+   void showString()
+    {
+        int i = 0;
+        while (*(array + i))
+        {
+            cout << *(array + i);
+            i++;
+        }
+        cout << endl;
+       
+   
+    }
+
+
+
+    friend void operator << (ostream& output,  String& s)
+    {
+        output << s.array<<endl;
+    }
+
+      void operator - (int i)
+    {
+        for (int k = i;k < strlen(array);k++)
+        {
+            array[k] = array[k + 1];
+        }
+        *(array + strlen(array) ) = '\0';
+    }
+
+      bool operator > (char *p)
+      {
+          int counter = 0;
+             
+
+             for (int k = 0;k < strlen(array);k++)
+                      {
+
+                          if (array[k] == p[counter])
+                          {
+                              counter++;
+                                  if (counter == strlen(p))
+                                      {
+                                          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 4) | 10));
+                                          cout << " В " << typeid(String).name() << " входит строка '"<<p<<"' \n";
+                                          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+                                          return true;
+                                      }
+                          }
+                          else counter = 0;
+                      }
+
+        
+             SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+              cout << "Не входит\n";
+                 SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+              return false;
+           
+      }
+
+
+      bool operator > (String p)
+      {
+          int counter = 0;
+
+          if (p.Length() <= this->Length())
+          {
+
+              for (int i(0);i < this->Length();i++)
+              {
+                  if (this->array[i] == p[counter])
+                  {
+                      counter++;
+                      if (counter == p.Length())
+                      {
+                          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 4) | 10));
+                          cout <<"В строку "<<typeid(*this).name()<< " Входит "<<typeid(&p).name()<<endl;
+                          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+                          return true;
+                      }
+                  }
+                  else counter = 0;
+              }
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+              cout << "Не входит\n";
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+              return false;
+          }
+          else
+          {
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+              cout << "Не входит\n";
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+              return false;
+          }
+      }
+
+
+      bool operator != (char *s)
+      {
+          if (this->Length() != strlen(s))
+          {
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+              cout << "Строки не равны (размер)\n";
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+              return true;
+          }
+
+          else
+          {
+              for (int i = 0;i < this->Length();i++)
+              {
+                  if (array[i] != s[i])
+                  {
+                      SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+                      cout << "Строки не равны (посимвольное сравнение)\n";
+                      SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+                      return true;
+                  }
+              }
+          }
+
+          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 4) | 10));
+          cout << "Строки идентичны\n";
+          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+          return false;
+      }
+
+
+
+      bool operator != (String s)
+      {
+          if (this->Length() != s.Length())
+          {
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+              cout << "Строки не равны (размер)\n";
+              SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+              return true;
+          }
+
+          else
+          {
+              for (int i = 0;i < this->Length();i++)
+              {
+                  if (array[i] != s[i])
+                  {
+                      SetConsoleTextAttribute(hConsole1, (WORD)((2 << 3) | 12));
+                      cout << "Строки не равны (посимвольное сравнение)\n";
+                      SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+                      return true;
+                  }
+              }
+          }
+
+          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 4) | 10));
+          cout << "Строки идентичны\n";
+          SetConsoleTextAttribute(hConsole1, (WORD)((2 << 2) | 7));
+          return false;
+      }
+
+      void operator+( char& p )
+      {
+          int i;
+          cout << "куда вставляем?" << endl;
+          cin >> i;
+          if (i > strlen(array)) { cout << "Не удалось выполнить вставку\n"; return; }
+
+          for (int j = strlen(this->array);j>=i;j--)
+          {
+              this->array[j + 1] = this->array[j];
+          }
+          this->array[i] = p;
+      }
+
+      int Length()
+      {
+          return strlen(array);
+      }
+   
+      void operator+(char s)
+      {
+       strcat(array , &s);
+      }
+
+      char operator[](int i)
+      {
+          if (i > this->Length())
+              return false;
+          else
+          return array[i];
+      }
+
+     
+};
---
+++ laborotary6/laborotary6/laborotary6.cpp
@@ -0,0 +1,26 @@
+// laborotary6.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Header.h"
+
+
+int main()
+{
+    setlocale(0, "");
+    String s1("123456");
+    String s2("124565");
+    char c('q');
+
+    //s1 - 2; cout << s1;
+    //s1 > s2;
+    //s1 > s2;
+   
+    /*bool a = s1 != "01234";
+    s1 != s2;*/
+   
+    s1 + 'qs';
+    cout << s1;
+    return 0;
+}
+
